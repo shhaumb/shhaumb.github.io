a95507d4cad182fb7537d3411af314798c1cfc60
Shubham Jain <sj.iitr@gmail.com>
2021-04-10 10:43:38 +0530

    Fix domain links

--- generate-site.sh
+++ generate-site.sh
@@ -1,3 +1,4 @@
 wget --recursive --page-requisites --html-extension --convert-links --restrict-file-names=windows --no-parent --no-host-directories http://localhost:8080/
 find . -type f -name "*.html" -exec sed -i.bk -e 's/\/index\.html\"/\/\"/g' {} \;
+find . -type f -name "*.html" -exec sed -i.bk -e 's/localhost:8080/thelastinfinity.com/g' {} \;
 find . -name "*.bk" -delete
