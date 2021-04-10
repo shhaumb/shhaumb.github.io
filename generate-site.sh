67be09d403af8e7a3e9352f22774fd9e5b19d225
Shubham Jain <sj.iitr@gmail.com>
2021-04-10 09:21:43 +0530

    fix links

--- generate-site.sh
+++ generate-site.sh
@@ -1,1 +1,3 @@
 wget --recursive --page-requisites --html-extension --convert-links --restrict-file-names=windows --no-parent --no-host-directories http://localhost:8080/
+find . -type f -name "*.html" -exec sed -i.bk -e 's/\/index\.html\"/\/\"/g' {} \;
+find . -name "*.bk" -delete
