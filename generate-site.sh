b3246dff7729b3fd990ef1fe7d3004a69c36451d
Shubham Jain <sj.iitr@gmail.com>
2021-04-10 08:59:01 +0530

    move site

--- generate-site.sh
+++ generate-site.sh
@@ -1,1 +1,1 @@
-wget --recursive --page-requisites --html-extension --convert-links --restrict-file-names=windows --no-parent --no-host-directories --directory-prefix=site http://localhost:8080/
+wget --recursive --page-requisites --html-extension --convert-links --restrict-file-names=windows --no-parent --no-host-directories http://localhost:8080/
